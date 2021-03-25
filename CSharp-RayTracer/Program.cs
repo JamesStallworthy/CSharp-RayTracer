06fa4c9ec3199a52d319154886cb4091a8026d2b
JamesStallworthy <james@jamesstallworthy.com>
2021-03-25 18:12:36 +0000

    Updated project to use .net5.0

--- CSharp-RayTracer/Program.cs
+++ CSharp-RayTracer/Program.cs
@@ -10,13 +10,12 @@ namespace CSharp_RayTracer
             WindowManager window = new WindowManager(500,500, "CSharp_RayTracer");
         
             Scene s = new Scene();
-
-            //s.AddShapeToScene(new Sphere(new Vector3(5,5,50),10,new MaterialSolidShaded(new Colour(0,255,0))));
             
             s.AddShapeToScene(new Sphere(new Vector3(20,0,40),10,new MaterialPhong(new Colour(0,0,255),0.5f,0.08f,0.5f,500.0f)));
             s.AddShapeToScene(new Sphere(new Vector3(0,0,40),10,new MaterialPhong(new Colour(0,255,0),0.5f,0.08f,0.5f,50.0f)));
+            s.AddShapeToScene(new Sphere(new Vector3(-20,0,41),10,new MaterialSolidShaded(new Colour(255,0,0))));
 
-            s.SetLightSource(new PointLight(new Vector3(5,20,20)));
+            s.SetLightSource(new PointLight(new Vector3(10,0,20)));
 
             Renderer renderer = new Renderer(new Vector3(0,0,0),500,500,80,s);
             window.Render(renderer.Render());
